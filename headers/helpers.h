bab2637e81d7e4844bfd40c10887e80fdd9607c3
Polydoros Giannouris <92758972+PolydorosG@users.noreply.github.com>
2021-12-06 19:24:57 +0200

    Add files via upload

---
+++ headers/helpers.h
@@ -0,0 +1,11 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+float calculateDistanceArray(float *p, int start, float *ref, uint dims);
+
+uint partition(float *arr, uint low, uint high);
+float kthSmallest(float *array, uint start, uint end, uint k);
+float quickselect(float *distances, uint end);
+
+
+#endif
