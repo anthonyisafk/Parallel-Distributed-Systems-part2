3f9348d0cbe5e9a85b4284de90cf59c5091d38f2
anthonyisafk <tonytonytony555@live.com>
2021-12-07 23:43:11 +0000

    Create the process struct header file

---
+++ headers/process.h
@@ -0,0 +1,22 @@
+/**
+ * @file: process.h
+ * ********************
+ * @description: Describes a process struct, that is used to shorten 
+ * the arguments list for the mpihelp header file. 
+ */ 
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <mpi.h>
+
+typedef struct {
+    int comm_size;
+    int comm_rank;
+    long dims;
+    long pointsNum;
+
+    MPI_Status *mpi_stat101;
+} process;
+
+#endif
